227ee242b5878aea7712240900db2245b18d60ce
Levy <kipkoechlevy096@gmail.com>
2023-01-19 14:14:37 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,29 @@
-import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
+import { useState } from 'react';
 import NavBar from "./NavBar";
 import MoviesPage from "./MoviesPage";
 
 function App() {
-  const [movies, setMovies] = useState({
-    1: { id: 1, title: "A River Runs Through It" },
-    2: { id: 2, title: "Se7en" },
-    3: { id: 3, title: "Inception" },
-  });
+const [movies, setMovies] = useState({
+1: { id: 1, title: "A River Runs Through It" },
+2: { id: 2, title: "Se7en" },
+3: { id: 3, title: "Inception" },
+});
 
-  return (
-    <div>
-      <NavBar />
-      <Switch>
-        <Route path="/movies">
-          <MoviesPage movies={movies} />
-        </Route>
-        <Route exact path="/">
-          <div>Home</div>
-        </Route>
-      </Switch>
-    </div>
-  );
+return (
+<div>
+<NavBar />
+<Switch>
+<Route path="/movies">
+<MoviesPage movies={movies} setMovies={setMovies} />
+</Route>
+<Route exact path="/">
+<div>Home</div>
+</Route>
+</Switch>
+</div>
+);
 }
 
 export default App;
+
